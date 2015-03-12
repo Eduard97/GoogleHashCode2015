0479f38ff885a16af62105191571360dc66a5042
Gerome Egron <legunda.gg&gmail.com>
2015-03-12 19:22:57 +0100

    Add exec plugin to launch software

---
+++ README.md
@@ -0,0 +1,4 @@
+Usage:
+mvn exec:java -Dexec.args=[FILE_PATH]
+
+Will generate a file with the solution.
--- src/main/java/fr/xebia/google/hashcode/Launcher.java
+++ src/main/java/fr/xebia/google/hashcode/Launcher.java
@@ -1,4 +1,11 @@
 package fr.xebia.google.hashcode;
 
 public class Launcher {
+
+    public static void main(String[] args) {
+        String filePath = args[0];
+
+        System.out.println(filePath);
+    }
+
 }
