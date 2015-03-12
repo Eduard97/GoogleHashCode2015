4d943884a7be5aebeed0a11a0fa73493b301ee7c
mdulac <mat.dulac@gmail.com>
2015-03-12 19:35:52 +0100

    add servers into datacenter

--- src/main/java/fr/xebia/google/hashcode/model/DataCenter.java
+++ src/main/java/fr/xebia/google/hashcode/model/DataCenter.java
@@ -7,17 +7,30 @@ public class DataCenter {
 
     private List<Row> rows = new ArrayList<>();
 
+    private List<Server> servers = new ArrayList<>();
+
     public List<Row> getRows() {
         return rows;
+    }
+
+    public List<Server> getServers() {
+        return servers;
     }
 
     public void addRow(Row row) {
         this.rows.add(row);
     }
 
+    public void addServer(Server server) {
+        this.servers.add(server);
+    }
+
+    public void addServers(List<Server> servers) {
+        this.servers = servers;
+    }
+
     public void setUnavailableAt(int rowIndex, int location) {
         Row row = rows.get(rowIndex);
         row.locations[location] = State.NOT_AVAILABLE;
     }
-
 }
