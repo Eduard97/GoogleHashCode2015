bd25718a539412210d6f6ec5736cd85d1199ea78
gegron <gerome.egron@gmail.com>
2015-03-13 12:19:36 +0100

    init computeScore

---
+++ src/main/java/fr/xebia/google/hashcode/processor/ScoreComputer.java
@@ -0,0 +1,11 @@
+package fr.xebia.google.hashcode.processor;
+
+import fr.xebia.google.hashcode.model.DataCenter;
+
+public class ScoreComputer {
+
+    public static Integer compute(DataCenter dataCenter) {
+        return null;
+    }
+
+}
